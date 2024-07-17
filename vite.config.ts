54d0a6389e2b8a1bac27833da62801166e10713b
HotariTobu <hotari24tools@gmail.com>
2024-07-17 17:35:44 +0900

    fix: pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'dnd-kit-multiple-columns-sample'
+  base: '/dnd-kit-multiple-columns-sample/'
 });
